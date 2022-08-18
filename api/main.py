e451395afdff7468e5b740052fde41d2f8687ade
bsxi <bsxi@protonmail.com>
2022-08-19 00:22:37 +0100

    Updated main api module to parse data as journey models

--- api/main.py
+++ api/main.py
@@ -1,34 +1,110 @@
-from requests.auth import HTTPBasicAuth
+"""Module to handle train journeys from realtraintimes api"""
 from datetime import date
+import logging
+import time
 import requests
+from requests.auth import HTTPBasicAuth
+from model.journey import Journey
 
-# Train Data SOurced from Real Train TImes API
-username = "rttapi_mpvii"
-password = "f63e45c3ffd06e42cd5d77f5b371be72497172fb"
+# Train data sourced from RealTrainTimes API
+USER = "rttapi_mpvii"
+PASS = "f63e45c3ffd06e42cd5d77f5b371be72497172fb"
+ENDPOINT = "https://api.rtt.io/api/v1"
+FORMAT = "json"
 
-# url = "https://api.rtt.io/api/v1/json/search/MDL/to/HAZ"
+# Get today's date in YYYY/MM/DD
+DATE = date.today().strftime("%Y/%m/%d")
 
-# Get Today's dat in YYYY/MM/DD
-# today = date.today().strftime("%Y/%m/%d")
-today = "2022/06/24/0815"
+# Set our use-case services that impact a crossing
+SERVICES = [
+    ["DSL", "HAZ"],
+    ["HAZ", "DSL"],
+    ["HAZ", "MDL"],
+    ["MDL", "HAZ"]
+]
 
-endpoint = "https://api.rtt.io/api/v1"
+# Log level set for testing
+logging.basicConfig(level=logging.DEBUG)
 
-call = f"/json/search/MDL/to/MAN/{today}"
+def get_journeys(start_pos, end_pos, journey_date):
+    """
+    Returns information on train journeys from one station to another for a given day.
 
+    Args:
+        start_pos (str): Code of the station where the journey begins.
+        end_pos (str): Code of the station where the journey ends.
+        journey_date (str): The required date to check for journeys. In format YYYY/MM/DD.
 
+    Returns:
+        list: A list of journey objects.
+    """
+    logging.info(f"Getting journeys for {start_pos}-{end_pos}")
 
-response = requests.get(endpoint+call, auth=HTTPBasicAuth(username, password))
-journeys = response.json()['services']
+    outbound_data = requests.get(
+        f"{ENDPOINT}/{FORMAT}/search/{start_pos}/to/{end_pos}/{journey_date}",
+        auth=HTTPBasicAuth(USER, PASS)
+    ).json()['services']
 
-for journey in journeys:
-    print(journey['locationDetail']['realtimeDeparture'])
+    # Sleeping for any weird shit on the api end
+    time.sleep(2)
 
+    arrival_data = requests.get(
+        f"{ENDPOINT}/{FORMAT}/search/{end_pos}/from/{start_pos}/{journey_date}",
+        auth=HTTPBasicAuth(USER, PASS)
+    ).json()['services']
 
+    # Clean arrival data
+    # Data in the past is very scuffed for whatever reason
+    # Needs investigating, sometimes the 'arrival_data' will show services
+    # that are not from the correct station.
+    # Other times the arrival/outbound data will be completely off.
+    # This is recorded around a day of train strikes, so not sure how much impact that's had.
+    # Or it could be that actual times are being updated for one station, and not another
 
-call2 = f"/json/search/HAZ/to/MAN/{today}"
-response2 = requests.get(endpoint+call2, auth=HTTPBasicAuth(username, password))
-print(response2.content)
+    # This step cleans some of those situations up, but it needs to be figured out.
+    logging.info("Cleaning arrival data")
+    service_ids = []
 
+    for service in outbound_data:
+        service_ids.append(service['serviceUid'])
 
+    arrival_data = [item for item in arrival_data if item['serviceUid'] in service_ids]
 
+    journeys = []
+
+    # Parse API data into a reusable object
+    for i in range(len(outbound_data)):
+        journeys.append(Journey(
+            start_pos,
+            end_pos,
+            journey_date,
+            int(outbound_data[i]['locationDetail']['realtimeDeparture']),
+            int(arrival_data[i]['locationDetail']['realtimeArrival'])
+        ))
+
+    return journeys
+
+def get_all_journeys(services, journey_date):
+    """
+    Returns a list of all possible impacting journeys.
+
+    Args:
+        services (list): A list of services to get journeys for.
+        journey_date (str): The required date to check for journeys. In format YYYY/MM/DD.
+
+    Returns:
+        list: A list of journey objects.
+    """
+    journeys = []
+
+    for service in services:
+        journeys.extend(get_journeys(service[0], service[1], journey_date))
+
+    return journeys
+
+# Testing
+j = get_all_journeys(SERVICES, "2022/08/19")
+
+for journey in j:
+    print(f"Journey from {journey.origin} to {journey.destination}, \
+departing at {journey.departure_time} & arriving at {journey.arrival_time}")
