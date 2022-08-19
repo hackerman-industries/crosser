8ed236501d45acb041f2d63f7eecae08965158df
bsxi <bsxi@protonmail.com>
2022-08-19 11:40:26 +0100

    Added model for a train crossing

--- api/model/journey.py
+++ api/model/journey.py
@@ -13,13 +13,3 @@ class Journey:
     def calculate_journey_length(self):
         return self.arrival_time - self.departure_time
 
-class CrossingJourney(Journey):
-    """
-    Class to represent a train journey that impacts a crossing.
-    """
-    def __init__(self, origin, destination, journey_date, departure_time, arrival_time) -> None:
-        Journey.__init__(self, origin, destination, journey_date, departure_time, arrival_time)
-        #self.crossing_time = self.calculate_crossing_time()
-
-    def calculate_crossing_time(self):
-        pass
